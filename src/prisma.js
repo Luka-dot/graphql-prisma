48f515738d4421a8ddde8b204910003d78a25d64
Luka-dot <79lukask@gmail.com>
2021-01-06 18:37:13 -0500

    mutation and call chaining

--- src/prisma.js
+++ src/prisma.js
@@ -7,12 +7,44 @@ const prisma = new Prisma({
     endpoint: 'http://localhost:4466/'
 })
 
-prisma.query.users(null, '{ id name posts { id title } }').then((data) => {
-   // console.log(JSON.stringify(data, undefined, 4))
-})
+// prisma.query.users(null, '{ id name posts { id title } }').then((data) => {
+//     console.log(JSON.stringify(data, undefined, 4))
+// })
 
-prisma.query.comments(null, '{ id text author { id name } }').then((data) => {
-    console.log(JSON.stringify(data, undefined,4))
+// prisma.query.comments(null, '{ id text author { id name } }').then((data) => {
+//     console.log(JSON.stringify(data, undefined,4))
+// })
+
+// prisma.mutation.createPost({
+//     data: {
+//         title: "2nd post by Node.js",
+//         body: "second post body is here!!!!",
+//         published: true,
+//         author: {
+//             connect: {
+//                 id: "ckjljb8ad00g208087k1rrf71"
+//             }
+//         }
+//     }
+// }, '{ id title body published }').then((data) => {
+//     console.log(JSON.stringify(data, undefined, 2))
+//     return prisma.query.users(null, '{ id name posts { id title } }')
+// }).then((data) => {
+//     console.log(JSON.stringify(data, undefined, 4))
+// })
+
+prisma.mutation.updatePost({
+    data: {
+        published: true
+    },
+    where: {
+        id:"ckjm0wy0900wo0808xt0yc3uj"
+    }
+}, '{ id published }').then((data) => {
+    console.log(JSON.stringify(data, undefined, 2))
+    return prisma.query.posts(null, '{ id title body published }')
+}).then((data) => {
+    console.log(JSON.stringify(data, undefined, 2))
 })
 
 
