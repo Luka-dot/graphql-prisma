a2bf7a8ec43d7ea037db6ac94ee8a92094c4b9f8
Luka-dot <79lukask@gmail.com>
2021-01-27 10:47:54 -0500

    expiration utility file created

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
 import getUserId from '../utils/getUserId'
+import generateToken from '../utils/generateToken'
 
 const Mutation = {
     async createUser(parent, args, { prisma }, info) {
@@ -19,7 +19,7 @@ const Mutation = {
 
         return {
             user,
-            token: jwt.sign({ userId: user.id }, 'thisissecret', { expiresIn: '7 days' })
+            token: generateToken(user.id)
         }
     },
     async deleteUser(parent, args, { prisma, request }, info) {
@@ -60,7 +60,7 @@ const Mutation = {
 
         return {
             user,
-            token: jwt.sign({ userId: user.id }, 'thisissecret', { expiresIn: '7 days' })
+            token: generateToken(user.id)
         }
     },
     createPost(parent, args, { prisma, request }, info) {
