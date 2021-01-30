cf8cb99bc5cf75ea2e7a0706118423e03c4dca5c
Luka-dot <79lukask@gmail.com>
2021-01-29 21:58:29 -0500

    test cases added

--- src/utils/user.js
+++ src/utils/user.js
@@ -2,4 +2,8 @@ const getFirstName = (fullname) => {
     return fullname.split(' ')[0]
 }
 
-export { getFirstName }
+const isValidPassword = (password) => {
+    return password.length >= 8 && !password.toLowerCase().includes('password')
+}
+
+export { getFirstName, isValidPassword }
--- tests/user.test.js
+++ tests/user.test.js
@@ -1,6 +1,32 @@
-import { getFirstName } from '../src/utils/user'
+import { extractFragmentReplacements } from 'prisma-binding'
+import { getFirstName, isValidPassword } from '../src/utils/user'
 
 test('should return first name out of full name', () => {
     const firstName = getFirstName('Lukas kotrc')
     
+    expect(firstName).toBe('Lukas')
+})
+
+test('should return first name if only first name was given', () => {
+    const firstName = getFirstName('Jen')
+
+    expect(firstName).toBe('Jen')
+})
+
+test('should reject password shorter 8 characters', () => {
+    const password = isValidPassword('pas123')
+
+    expect(password).toBe(false)
+})
+
+test('should reject password that includes world password', () => {
+    const password = isValidPassword('Password')
+
+    expect(password).toBe(false)
+})
+
+test('should password be at least 8 characters', () => {
+    const password = isValidPassword('pas123456')
+
+    expect(password).toBe(true)
 })
